f26066bb5929712cc594590bf7734a93a27aff23
EstrellaDionis <EstrellaDionis@gmail.com>
2022-03-23 19:58:06 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/shopApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO Connection Open!");
+  })
+  .catch((err) => {
+    console.log("Oh No! MONGO connection Error!");
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/dog", (req, res) => {
+  res.send("Woof!");
+});
+
+app.listen(9000, () => {
+  console.log("App is listening on port 9000!");
+});
