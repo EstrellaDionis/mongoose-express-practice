47cd5b74b32b265fddd57effd4c3799153e6ecff
EstrellaDionis <EstrellaDionis@gmail.com>
2022-03-24 14:10:14 -0400

    created index.js in products folder and using ejs in index.ejs and show.ejs to render products to /products/:id endpoint

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ app.get("/products", async (req, res) => {
   res.render("products/index", { products });
 });
 
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  console.log(product);
+  res.render("products/show", { product });
+});
+
 app.listen(9000, () => {
   console.log("App is listening on port 9000!");
 });
